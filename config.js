d68b0209190f069fa8dd9b1592b1ba37ba8cfc91
joshuahiskraken <never.noes.best@gmail.com>
2018-06-02 05:50:52 -0500

    Not working, no console log, no errors.

--- config.js
+++ config.js
@@ -1,1 +1,16 @@
 //config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key Consumer Key (API Key)	
+ * consumer_secret Consumer Secret (API Secret)	SiDSBHD4gSN1CPa7cU03UF7jyqXXKLHddgCcnFeXJYwe1iod9D
+
+ * access_token Access Token	1270921964-z2N9V61rP0UMbtOqlzWSnvU4kXSFhDPce1HDVXb
+ * access_token_secret Access Token Secret	avcMzYVtBxivQgTR1SAlIoC7gGeTdVGEV8rOyM53StPu5
+
+ */
+
+module.exports = {
+    consumer_key: 'jVPPXvlZq3VNmxnbwFzqSg8kO',
+    consumer_secret: 'SiDSBHD4gSN1CPa7cU03UF7jyqXXKLHddgCcnFeXJYwe1iod9D',
+    access_token: '1270921964-z2N9V61rP0UMbtOqlzWSnvU4kXSFhDPce1HDVXb',
+    access_token_secret: 'avcMzYVtBxivQgTR1SAlIoC7gGeTdVGEV8rOyM53StPu5'
+}
