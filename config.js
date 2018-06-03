1041a00add0a526f257fe83cb14fb6718918dccb
joshuahiskraken <never.noes.best@gmail.com>
2018-06-03 01:15:57 +0000

    new commit

--- config.js
+++ config.js
@@ -1,16 +1,10 @@
 //config.js
-/** TWITTER APP CONFIGURATION
- * consumer_key Consumer Key (API Key)	
- * consumer_secret Consumer Secret (API Secret)	SiDSBHD4gSN1CPa7cU03UF7jyqXXKLHddgCcnFeXJYwe1iod9D
 
- * access_token Access Token	1270921964-z2N9V61rP0UMbtOqlzWSnvU4kXSFhDPce1HDVXb
- * access_token_secret Access Token Secret	avcMzYVtBxivQgTR1SAlIoC7gGeTdVGEV8rOyM53StPu5
-
- */
+require('dotenv').config()
 
 module.exports = {
-    consumer_key: 'jVPPXvlZq3VNmxnbwFzqSg8kO',
-    consumer_secret: 'SiDSBHD4gSN1CPa7cU03UF7jyqXXKLHddgCcnFeXJYwe1iod9D',
-    access_token: '1270921964-z2N9V61rP0UMbtOqlzWSnvU4kXSFhDPce1HDVXb',
-    access_token_secret: 'avcMzYVtBxivQgTR1SAlIoC7gGeTdVGEV8rOyM53StPu5'
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
 }
